839345b52a2298ffe159bedc3b641091ccdd8c21
HoomanFarhadi <45983177+HoomanFarhadi@users.noreply.github.com>
2021-12-04 03:01:37 -0500

    Update __init__.py

--- gym_snake/__init__.py
+++ gym_snake/__init__.py
@@ -1,1 +1,10 @@
+from gym.envs.registration import register
 
+register(
+    id='snake-v0',
+    entry_point='snake_foo.envs:SnakeEnv',
+)
+register(
+    id='snake-extrahard-v0',
+    entry_point='gym_snake.envs:SnakeExtraHardEnv',
+)
